7b889be9fc930d5989a744fb21369d8f06604c0a
noyamlv <ejeratio@gmail.com>
2025-09-08 14:20:58 +0800

    added practice exrcises

---
+++ public/tuff.js
@@ -0,0 +1,19 @@
+console.log(1)
+console.log(2)
+console.log(3)
+
+for (let i = 1; i <= 3; i++){
+    console.log(i)
+}
+
+i = 1;
+while (i <= 3){
+    console.log(i)
+    i++
+}
+
+i = 1;
+do {
+    console.log(i)
+    i++
+} while (i <= 3);
