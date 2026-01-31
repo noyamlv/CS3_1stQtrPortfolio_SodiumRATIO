03c1aa51a9cbb7e737232105babd8873e15162c3
noyamlv <ejeratio@gmail.com>
2026-01-31 22:32:14 +0800

    at hahalik ka nang may lipstick na dugo

--- public/q3_2gszj.js
+++
@@ -1,8 +0,0 @@
-    function checkEmpty(input) {
-        if (input.value.trim() === "") {
-            input.classList.add("empty");
-        } else {
-            input.classList.remove("empty");
-        }
-    }
-
